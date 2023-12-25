c1bb4854900a30332f6a543008685277c61e1e07
unknown <mayuresh.madiwale@yahoo.com>
2023-12-25 15:22:27 +0530

    dvc added

--- src/cnnClassifier/pipeline/stage_04_evaluation.py
+++ src/cnnClassifier/pipeline/stage_04_evaluation.py
@@ -23,6 +23,6 @@ if __name__ == "__main__":
         logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<<")
         obj = EvaluationPipeline()
         obj.main()
-        logger.info(">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx===================x")
+        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx===================x")
     except Exception as e:
         raise e
